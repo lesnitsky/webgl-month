84bc1ea0650cb9160a667b490689577b185efac0
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-01 17:11:36 +0300

    Now we can fill each pixel storage with colors. Note that alpha component is also in  range unlike CSS

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -5,4 +5,11 @@ const ctx = canvas.getContext('2d');
 
 function fillRect(top, left, width, height) {
     const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
+
+    for (let i = 0; i < pixelStore.length; i += 4) {
+        pixelStore[i] = 0; // r
+        pixelStore[i + 1] = 0; // g
+        pixelStore[i + 2] = 0; // b
+        pixelStore[i + 3] = 255; // alpha
+    }
 }
