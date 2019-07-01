487a2f2ddf4e5b7b253bd136654dc1a8f21635d6
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-01 17:02:19 +0300

    Let's implement our own version of

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -3,4 +3,6 @@ console.log('Hello WebGL month');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillRect(0, 0, 100, 50);
+function fillRect(top, left, width, height) {
+
+}
