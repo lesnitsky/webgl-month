0ded3ffaf5e3de7816eda8a4373d309f9c7122ca
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-01 17:24:45 +0300

    and iterate over these pixels instead of the whole canvas

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -21,16 +21,18 @@ function calculatePixelIndices(top, left, width, height) {
 
 function fillRect(top, left, width, height) {
     const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
+    
+    const pixelIndices = calculatePixelIndices(top, left, width, height);
 
-    for (let i = 0; i < pixelStore.length; i += 4) {
+    pixelIndices.forEach((i) => {
         pixelStore[i] = 0; // r
         pixelStore[i + 1] = 0; // g
         pixelStore[i + 2] = 0; // b
         pixelStore[i + 3] = 255; // alpha
-    }
+    });
 
     const imageData = new ImageData(pixelStore, canvas.width, canvas.height);
     ctx.putImageData(imageData, 0, 0);
 }
 
-fillRect();
+fillRect(10, 10, 100, 50);
