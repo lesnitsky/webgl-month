b3fa0ea6aeec71d8490142d5a11325747a934cd7
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-01 17:22:39 +0300

    Calculate pixel indices inside rectangle

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -3,6 +3,22 @@ console.log('Hello WebGL month');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+function calculatePixelIndices(top, left, width, height) {
+    const pixelIndices = [];
+
+    for (let x = left; x < left + width; x++) {
+        for (let y = top; y < top + height; y++) {
+            const i =
+                y * canvas.width * 4 + // pixels to skip from top
+                x * 4; // pixels to skip from left
+
+            pixelIndices.push(i);
+        }
+    }
+
+    return pixelIndices;
+}
+
 function fillRect(top, left, width, height) {
     const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
 
