a1aa2c8a3b65d98ee014ac6972f2ab989082918f
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-01 17:03:05 +0300

    So basically each pixel is just a color encoded in 4 integers. R, G, B channel and Alpha. To store info about each pixel of canvas we'll need a `Uint8ClampedArray`. The size of this array is `canvas.width * canvas.height` (pixels count) `* 4` (each pixel has 4 channels).

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -4,5 +4,5 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 function fillRect(top, left, width, height) {
-
+    const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
 }
