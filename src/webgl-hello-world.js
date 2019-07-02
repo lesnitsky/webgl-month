2a59c02bab27211a7fda65fcb8fc541edc77d666
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-02 18:43:05 +0300

    GPU program consists of two "functions" These functions are called `shaders` WebGL supports several types of shaders
    In this example we'll work with `vertex` and `fragment` shaders.
    Both could be created with `createShader` method

--- src/webgl-hello-world.js
+++ src/webgl-hello-world.js
@@ -2,3 +2,6 @@ const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
 const program = gl.createProgram();
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
