12752217453594773fa6573533fea80334e7556f
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-02 18:44:57 +0300

    Now let's write the simpliest possible shader

--- src/webgl-hello-world.js
+++ src/webgl-hello-world.js
@@ -5,3 +5,9 @@ const program = gl.createProgram();
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+const vShaderSource = `
+void main() {
+    
+}
+`;
