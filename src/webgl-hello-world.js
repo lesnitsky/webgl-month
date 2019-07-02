ec83b46e2e6d5f26587f36512df9c9c63da01c91
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-02 19:44:25 +0300

    To make any changes to GPU buffers, we need to "bind" it. After buffer is bound, it is treated as "current", and any buffer modification operation will be performed on "current" buffer.

--- src/webgl-hello-world.js
+++ src/webgl-hello-world.js
@@ -48,4 +48,6 @@ const positionData = new Float32Array([0, 0]);
 
 const positionBuffer = gl.createBuffer(gl.ARRAY_BUFFER);
 
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
 gl.drawArrays(gl.POINTS, 0, 1);
