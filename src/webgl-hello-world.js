073746d48e2ed430c58563c613601dc703121837
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-02 19:38:42 +0300

    Now let's pass point coordinate from JS instead of hardcoding it inside shader
    Input data of vertex shader is called `attribute`
    Let's define `position` attribute

--- src/webgl-hello-world.js
+++ src/webgl-hello-world.js
@@ -7,9 +7,11 @@ const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
 const vShaderSource = `
+attribute vec2 position;
+
 void main() {
     gl_PointSize = 20.0;
-    gl_Position = vec4(0, 0, 0, 1);
+    gl_Position = vec4(position.x, position.y, 0, 1);
 }
 `;
 
