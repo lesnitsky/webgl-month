82a89d079f0f446e75cb12b2d5464aa2766f31b3
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 23:11:21 +0200

    ### Inverse
    The very first and simple filter might be inverse all colors of the image.
    
    How do we inverse colors?
    
    Original values are in range `[0..1]`
    
    If we subtract from each component `1` we'll get negative values, there's an `abs` function in glsl
    
    You can also define other functions apart of `void main` in glsl pretty much like in C/C++, so let's create `inverse` function

--- src/shaders/texture.f.glsl
+++ src/shaders/texture.f.glsl
@@ -3,6 +3,10 @@ precision mediump float;
 uniform sampler2D texture;
 uniform vec2 resolution;
 
+vec4 inverse(vec4 color) {
+    return abs(vec4(color.rgb - 1.0, color.a));
+}
+
 void main() {
     vec2 texCoord = gl_FragCoord.xy / resolution;
     gl_FragColor = texture2D(texture, texCoord);
