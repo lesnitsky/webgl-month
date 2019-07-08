0db60c17e240bc76ac98da3b7abc7629c85f45e2
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 02:55:12 +0200

    Create shaders

--- src/texture.js
+++ src/texture.js
@@ -1,5 +1,12 @@
 import vShaderSource from './shaders/texture.v.glsl';
 import fShaderSource from './shaders/texture.f.glsl';
+import { compileShader } from './gl-helpers';
 
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
+
+const vShader = gl.createShader(gl.VERTEX_SHADER);
+const fShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+compileShader(gl, vShader, vShaderSource);
+compileShader(gl, fShader, fShaderSource);
