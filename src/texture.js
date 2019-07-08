ef05d871746e7c5736d24c36ee500865bcd35d55
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 02:58:02 +0200

    Create a vertex position buffer and fill it with data

--- src/texture.js
+++ src/texture.js
@@ -1,6 +1,8 @@
 import vShaderSource from './shaders/texture.v.glsl';
 import fShaderSource from './shaders/texture.f.glsl';
 import { compileShader } from './gl-helpers';
+import { createRect } from './shape-helpers';
+
 
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
@@ -18,3 +20,9 @@ gl.attachShader(program, fShader);
 
 gl.linkProgram(program);
 gl.useProgram(program);
+
+const vertexPosition = new Float32Array(createRect(-1, -1, 2, 2));
+const vertexPositionBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexPosition, gl.STATIC_DRAW);
