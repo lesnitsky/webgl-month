f77036315cd305502874faf4d04a4fdd5ef0c8ba
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 23:35:22 +0200

    A naive and simple implementation will be to interpolate original color with sepia color by a certain factor. There is a `mix` function for this

--- src/shaders/texture.f.glsl
+++ src/shaders/texture.f.glsl
@@ -13,11 +13,15 @@ vec4 blackAndWhite(vec4 color) {
 
 vec4 sepia(vec4 color) {
     vec3 sepiaColor = vec3(112, 66, 20) / 255.0;
+    return vec4(
+        mix(color.rgb, sepiaColor, 0.4),
+        color.a
+    );
 }
 
 void main() {
     vec2 texCoord = gl_FragCoord.xy / resolution;
     gl_FragColor = texture2D(texture, texCoord);
 
-    gl_FragColor = blackAndWhite(gl_FragColor);
+    gl_FragColor = sepia(gl_FragColor);
 }
