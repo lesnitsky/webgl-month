8c5c746827eba5e4d0c27a44a95094d30ce56541
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 23:17:13 +0200

    ### Black and White
    Let's think of how to implement black and white filter.
    
    White color is `vec4(1, 1, 1, 1)`
    
    Black is `vec4(0, 0, 0, 1)`
    
    What are shades of gray? Aparently we need to add the same value to each color component.
    
    So basically we need to calculate the "brightness" value of each component. In very naive implmentation we can just add all color components and divide by 3 (arithmetical mean).
    
    > Note: this is not the best approach, as different colors will give the same result (eg. vec3(0.5, 0, 0) and vec3(0, 0.5, 0), but in reality these colors have different "brightness", I'm just trying to keep these examples simple to understand)
    
    Ok, let's try to implement this

--- src/shaders/texture.f.glsl
+++ src/shaders/texture.f.glsl
@@ -7,9 +7,13 @@ vec4 inverse(vec4 color) {
     return abs(vec4(color.rgb - 1.0, color.a));
 }
 
+vec4 blackAndWhite(vec4 color) {
+    return vec4(vec3(1.0, 1.0, 1.0) * (color.r + color.g + color.b) / 3.0, color.a);
+}
+
 void main() {
     vec2 texCoord = gl_FragCoord.xy / resolution;
     gl_FragColor = texture2D(texture, texCoord);
 
-    gl_FragColor = inverse(gl_FragColor);
+    gl_FragColor = blackAndWhite(gl_FragColor);
 }
