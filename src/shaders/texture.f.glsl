4e0c5659f039c035b2917f3a13e18166b021f803
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 23:29:56 +0200

    Let's define `sepia` function and color

--- src/shaders/texture.f.glsl
+++ src/shaders/texture.f.glsl
@@ -11,6 +11,10 @@ vec4 blackAndWhite(vec4 color) {
     return vec4(vec3(1.0, 1.0, 1.0) * (color.r + color.g + color.b) / 3.0, color.a);
 }
 
+vec4 sepia(vec4 color) {
+    vec3 sepiaColor = vec3(112, 66, 20) / 255.0;
+}
+
 void main() {
     vec2 texCoord = gl_FragCoord.xy / resolution;
     gl_FragColor = texture2D(texture, texCoord);
