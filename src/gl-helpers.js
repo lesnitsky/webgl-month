1c4f3078da113073b6cd0247a51f57527ec3d019
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 02:50:27 +0200

    [GTI] move compile shader to gl-helpers

---
+++ src/gl-helpers.js
@@ -0,0 +1,10 @@
+export function compileShader(gl, shader, source) {
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    const log = gl.getShaderInfoLog(shader);
+
+    if (log) {
+        throw new Error(log);
+    }
+}
