d442eb40ce7221d45216bd8975a0c2fa23959117
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 02:48:58 +0200

    Get the webgl context

--- src/texture.js
+++ src/texture.js
@@ -1,2 +1,5 @@
 import vShaderSource from './shaders/texture.v.glsl';
 import fShaderSource from './shaders/texture.f.glsl';
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
