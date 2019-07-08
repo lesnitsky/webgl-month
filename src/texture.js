1b7879fe44b495a10d376caf80c2eb28e84ff369
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-08 12:25:51 +0200

    We also need to specify different parameters of texture. We'll talk about this parameters in next tutorials.

--- src/texture.js
+++ src/texture.js
@@ -55,5 +55,10 @@ loadImage(textureImageSrc).then((textureImg) => {
         textureImg,
     );
 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
     gl.drawElements(gl.TRIANGLES, vertexIndices.length, gl.UNSIGNED_BYTE, 0);
 });
