d8b2eab67dd4e77d4030b8bac245b2c3acfa6532
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-09 21:46:24 +0200

    Ok, we rendered two rectangles, but they use the same texture. Let's add one more attribute which will specify which texture to use and pass this data to fragment shader via another varying

--- src/shaders/texture.v.glsl
+++ src/shaders/texture.v.glsl
@@ -1,10 +1,13 @@
 attribute vec2 position;
 attribute vec2 texCoord;
+attribute float texIndex;
 
 varying vec2 vTexCoord;
+varying float vTexIndex;
 
 void main() {
     gl_Position = vec4(position, 0, 1);
 
     vTexCoord = texCoord;
+    vTexIndex = texIndex;
 }
