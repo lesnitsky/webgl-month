b3721d5c09d1afa1729b6ad2407c4cac620067bc
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-13 17:08:38 +0300

    And multiply vertex positions

--- src/shaders/rotating-square.v.glsl
+++ src/shaders/rotating-square.v.glsl
@@ -4,5 +4,5 @@ uniform vec2 resolution;
 uniform mat2 rotationMatrix;
 
 void main() {
-    gl_Position = vec4(position / resolution * 2.0 - 1.0, 0, 1);
+    gl_Position = vec4((position / resolution * 2.0 - 1.0) * rotationMatrix, 0, 1);
 }
