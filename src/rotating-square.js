1c71190988c6960091c40f1ab2d3eb6555b5638c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-13 15:36:40 +0300

    Make canvas fullscreen

--- src/rotating-square.js
+++ src/rotating-square.js
@@ -4,3 +4,11 @@ import fShaderSource from './shaders/rotating-square.f.glsl';
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
+const width = document.body.offsetWidth;
+const height = document.body.offsetHeight;
+
+canvas.width = width * devicePixelRatio;
+canvas.height = height * devicePixelRatio;
+
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
