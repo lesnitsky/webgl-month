024186045a6ea86fa083f8432fd3b74d5b28f723
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-13 15:36:00 +0300

    Get WebGL context

--- src/rotating-square.js
+++ src/rotating-square.js
@@ -1,2 +1,6 @@
 import vShaderSource from './shaders/rotating-square.v.glsl';
 import fShaderSource from './shaders/rotating-square.f.glsl';
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
