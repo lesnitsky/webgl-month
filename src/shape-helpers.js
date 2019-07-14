65e2aec8d2f0b83c46fd7cf5e00c6a4b822ab10b
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-14 16:52:19 +0300

    [GTI] revert createRect

--- src/shape-helpers.js
+++ src/shape-helpers.js
@@ -1,34 +1,19 @@
-const Pi_4 = Math.PI / 4;
-
-export function createRect(top, left, width, height, angle = 0) {
-    const centerX = width / 2;
-    const centerY = height / 2;
-
-    const diagonalLength = Math.sqrt(centerX ** 2 + centerY ** 2);
-
-    const x1 = centerX + diagonalLength * Math.cos(angle + Pi_4);
-    const y1 = centerY + diagonalLength * Math.sin(angle + Pi_4);
-
-    const x2 = centerX + diagonalLength * Math.cos(angle + Pi_4 * 3);
-    const y2 = centerY + diagonalLength * Math.sin(angle + Pi_4 * 3);
-
-    const x3 = centerX + diagonalLength * Math.cos(angle - Pi_4);
-    const y3 = centerY + diagonalLength * Math.sin(angle - Pi_4);
-
-    const x4 = centerX + diagonalLength * Math.cos(angle - Pi_4 * 3);
-    const y4 = centerY + diagonalLength * Math.sin(angle - Pi_4 * 3);
-
+export function createRect(top, left, width, height) {
     return [
-        x1 + left, y1 + top,
-        x2 + left, y2 + top,
-        x3 + left, y3 + top,
-        x4 + left, y4 + top,
+        left, // x1
+        top, // y1
+        left + width, // x2
+        top, // y2
+        left, // x3
+        top + height, // y3
+        left + width, // x4
+        top + height, // y4
     ];
 }
 
 export function createHexagon(centerX, centerY, radius, segmentsCount) {
     const vertexData = [];
-    const segmentAngle =  Math.PI * 2 / (segmentsCount - 1);
+    const segmentAngle = (Math.PI * 2) / (segmentsCount - 1);
 
     for (let i = 0; i < Math.PI * 2; i += segmentAngle) {
         const from = i;
