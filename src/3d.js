4ef5e8f0457dd906ace15a62a4cd99de9c62980f
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-15 16:21:45 +0300

    Now we need to define vertex indices

--- src/3d.js
+++ src/3d.js
@@ -67,3 +67,12 @@ const cubeVertices = new Float32Array([
     -1.0, 1.0, 1.0,
     -1.0, 1.0, -1.0,
 ]);
+
+const indices = new Uint8Array([
+    0, 1, 2, 0, 2, 3,       // front
+    4, 5, 6, 4, 6, 7,       // back
+    8, 9, 10, 8, 10, 11,    // top
+    12, 13, 14, 12, 14, 15, // bottom
+    16, 17, 18, 16, 18, 19, // right
+    20, 21, 22, 20, 22, 23, // left
+]);
