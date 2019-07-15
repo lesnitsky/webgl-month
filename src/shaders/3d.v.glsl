38ba97b034ff8f3ca042d1a1f1c6a68aa3b43055
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-15 16:39:45 +0300

    and multiply every matrix.

--- src/shaders/3d.v.glsl
+++ src/shaders/3d.v.glsl
@@ -5,5 +5,5 @@ uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
 void main() {
-    gl_Position = vec4(position, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
