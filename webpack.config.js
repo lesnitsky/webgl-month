70fa051ab21514acc774542db98524777ac1c53c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-15 16:15:07 +0300

    We'll need a new entry point

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
         'week-1': './src/week-1.js',
         texture: './src/texture.js',
         'rotating-square': './src/rotating-square.js',
+        '3d': './src/3d.js',
     },
 
     output: {
