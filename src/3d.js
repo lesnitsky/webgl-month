f6a7626ad44078b50033687709cf7fb968128390
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-15 16:17:14 +0300

    We'll also need a familiar from previous tutorials boilerplate for our WebGL program

--- src/3d.js
+++ src/3d.js
@@ -1,1 +1,31 @@
-console.log('Hello 3d!');
+import vShaderSource from './shaders/3d.v.glsl';
+import fShaderSource from './shaders/3d.f.glsl';
+import { compileShader, setupShaderInput } from './gl-helpers';
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+const width = document.body.offsetWidth;
+const height = document.body.offsetHeight;
+
+canvas.width = width * devicePixelRatio;
+canvas.height = height * devicePixelRatio;
+
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
+
+const vShader = gl.createShader(gl.VERTEX_SHADER);
+const fShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+compileShader(gl, vShader, vShaderSource);
+compileShader(gl, fShader, fShaderSource);
+
+const program = gl.createProgram();
+
+gl.attachShader(program, vShader);
+gl.attachShader(program, fShader);
+
+gl.linkProgram(program);
+gl.useProgram(program);
+
+const programInfo = setupShaderInput(gl, program, vShaderSource, fShaderSource);
