b0b5c02abdf1200114ebde001a97ed2c11d196cb
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-16 15:05:41 +0300

    Shaders support "arrays" of uniforms, so we can pass our color palette to this array and use index to get a color out of it

--- src/shaders/3d.v.glsl
+++ src/shaders/3d.v.glsl
@@ -4,10 +4,11 @@ attribute float colorIndex;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
+uniform vec4 colors[6];
 
 varying vec4 vColor;
 
 void main() {
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    vColor = color;
+    vColor = colors[int(colorIndex)];
 }
