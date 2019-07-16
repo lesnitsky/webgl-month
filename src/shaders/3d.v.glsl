bb41dec34ee1d0af4ae1e66960d8c9fddd92e5a0
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-16 14:11:57 +0300

    Next we need to define an attribute to pass color from js to vertex shader, and varying to pass it from vertex to fragment shader

--- src/shaders/3d.v.glsl
+++ src/shaders/3d.v.glsl
@@ -1,9 +1,13 @@
 attribute vec3 position;
+attribute vec4 color;
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
+varying vec4 vColor;
+
 void main() {
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vColor = color;
 }
