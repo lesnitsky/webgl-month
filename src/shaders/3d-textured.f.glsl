82bcc3190e80be77a170286d016d3cbe56f3a156
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-20 16:25:37 +0300

    and read fragment colors from texture

--- src/shaders/3d-textured.f.glsl
+++ src/shaders/3d-textured.f.glsl
@@ -1,7 +1,8 @@
 precision mediump float;
+uniform sampler2D texture;
 
 varying vec2 vTexCoord;
 
 void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = texture2D(texture, vTexCoord);
 }
