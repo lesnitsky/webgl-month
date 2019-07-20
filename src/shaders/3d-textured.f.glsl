628d5a606a1a0b5b1744e11a94ee9fe31dc3cdeb
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-20 16:27:54 +0300

    Turns out that image is flipped when read by GPU, so we need to flip it back

--- src/shaders/3d-textured.f.glsl
+++ src/shaders/3d-textured.f.glsl
@@ -4,5 +4,5 @@ uniform sampler2D texture;
 varying vec2 vTexCoord;
 
 void main() {
-    gl_FragColor = texture2D(texture, vTexCoord);
+    gl_FragColor = texture2D(texture, vTexCoord * vec2(1, -1) + vec2(0, 1));
 }
