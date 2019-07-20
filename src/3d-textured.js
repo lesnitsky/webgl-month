2e0ed5609c37c0441c5c62ae8318ce8abcd48d69
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-20 14:57:20 +0300

    We'll need a canvas, webgl context and make canvas fullscreen

--- src/3d-textured.js
+++ src/3d-textured.js
@@ -1,1 +1,11 @@
-console.log('Hello textures');
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+const width = document.body.offsetWidth;
+const height = document.body.offsetHeight;
+
+canvas.width = width * devicePixelRatio;
+canvas.height = height * devicePixelRatio;
+
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
