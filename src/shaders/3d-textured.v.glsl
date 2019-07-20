4d1ca760cafe6e053eabcf6cc036b76d9a1b7c34
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-20 14:55:56 +0300

    Now let's create simple shaders to render a 3d object with solid color. [Learn more in this tutorial](https://dev.to/lesnitsky/webgl-month-day-15-rendering-a-3d-cube-190f)

---
+++ src/shaders/3d-textured.v.glsl
@@ -0,0 +1,9 @@
+attribute vec3 position;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+void main() {
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+}
