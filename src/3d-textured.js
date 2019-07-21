4bcec1116be26c021436438eb8fd78be09459972
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-21 19:34:32 +0300

    Day 21

--- src/3d-textured.js
+++ src/3d-textured.js
@@ -1,4 +1,4 @@
-import { mat4 } from 'gl-matrix';
+import { mat4, vec3 } from 'gl-matrix';
 
 import vShaderSource from './shaders/3d-textured.v.glsl';
 import fShaderSource from './shaders/3d-textured.f.glsl';
@@ -52,6 +52,7 @@ gl.vertexAttribPointer(programInfo.attributeLocations.texCoord, 2, gl.FLOAT, fal
 const viewMatrix = mat4.create();
 const projectionMatrix = mat4.create();
 
+<<<<<<< HEAD
 mat4.lookAt(
     viewMatrix,
     [0, 4, -7],
@@ -66,19 +67,53 @@ mat4.perspective(
     0.01,
     100,
 );
+=======
+mat4.perspective(projectionMatrix, (Math.PI / 360) * 90, canvas.width / canvas.height, 0.01, 100);
+>>>>>>> 947a17d... Day 21
 
 gl.uniformMatrix4fv(programInfo.uniformLocations.viewMatrix, false, viewMatrix);
 gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
 
 gl.viewport(0, 0, canvas.width, canvas.height);
 
+const matrices = [];
+const rotationMatrix = mat4.create();
+
+for (let i = -50; i < 50; i++) {
+    for (let j = -50; j < 50; j++) {
+        const matrix = mat4.create();
+
+        const position = [i * 2, (Math.floor(Math.random() * 2) - 1) * 2, j * 2];
+        mat4.fromTranslation(matrix, position);
+
+        mat4.fromRotation(rotationMatrix, Math.PI * Math.round(Math.random() * 4), [0, 1, 0]);
+        mat4.multiply(matrix, matrix, rotationMatrix);
+
+        matrices.push(matrix);
+    }
+}
+
+const cameraPosition = [0, 10, 0];
+const cameraFocusPoint = vec3.fromValues(30, 0, 0);
+const cameraFocusPointMatrix = mat4.create();
+
+mat4.fromTranslation(cameraFocusPointMatrix, cameraFocusPoint);
+
 function frame() {
-    mat4.rotateY(cube.modelMatrix, cube.modelMatrix, Math.PI / 180);
+    mat4.translate(cameraFocusPointMatrix, cameraFocusPointMatrix, [-30, 0, 0]);
+    mat4.rotateY(cameraFocusPointMatrix, cameraFocusPointMatrix, Math.PI / 360);
+    mat4.translate(cameraFocusPointMatrix, cameraFocusPointMatrix, [30, 0, 0]);
 
-    gl.uniformMatrix4fv(programInfo.uniformLocations.modelMatrix, false, cube.modelMatrix);
-    gl.uniformMatrix4fv(programInfo.uniformLocations.normalMatrix, false, cube.normalMatrix);
+    mat4.getTranslation(cameraFocusPoint, cameraFocusPointMatrix);
 
-    gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.data.length / 3);
+    mat4.lookAt(viewMatrix, cameraPosition, cameraFocusPoint, [0, 1, 0]);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.viewMatrix, false, viewMatrix);
+
+    matrices.forEach((matrix) => {
+        gl.uniformMatrix4fv(programInfo.uniformLocations.modelMatrix, false, matrix);
+
+        gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.data.length / 3);
+    });
 
     requestAnimationFrame(frame);
 }
