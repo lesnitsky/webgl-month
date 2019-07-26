93d479dcdc8ec3f1302f314e536a6805f0056b8c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-26 17:48:50 +0300

    Framebuffer itself is not a storage, but rather a set of references to "attachments" (color, depth)
    To render colors we'll need a texture

--- src/minecraft.js
+++ src/minecraft.js
@@ -31,6 +31,15 @@ mat4.fromTranslation(cameraFocusPointMatrix, cameraFocusPoint);
 
 const framebuffer = gl.createFramebuffer();
 
+const texture = gl.createTexture();
+
+gl.bindTexture(gl.TEXTURE_2D, texture);
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, canvas.width, canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
 function render() {
     mat4.translate(cameraFocusPointMatrix, cameraFocusPointMatrix, [0, 0, -30]);
     mat4.rotateY(cameraFocusPointMatrix, cameraFocusPointMatrix, Math.PI / 360);
