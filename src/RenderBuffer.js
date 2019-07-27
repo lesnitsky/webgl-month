2348347df50fd9c6c3d397e4272ed1b3f02d9be5
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-27 16:52:26 +0300

    Setup framebuffer and color texture

--- src/RenderBuffer.js
+++ src/RenderBuffer.js
@@ -2,5 +2,15 @@ export class RenderBuffer {
     constructor(gl) {
         this.framebuffer = gl.createFramebuffer();
         this.texture = gl.createTexture();
+
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.canvas.width, gl.canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.texture, 0);
     }
 }
