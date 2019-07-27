e13afa9b48f8233113eb2a0ba0138749d51b8432
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-27 17:00:05 +0300

    and convert this float to a color right in the shader

--- src/shaders/3d-textured.v.glsl
+++ src/shaders/3d-textured.v.glsl
@@ -8,6 +8,13 @@ uniform mat4 projectionMatrix;
 
 varying vec2 vTexCoord;
 
+vec3 encodeObject(float id) {
+    int b = int(mod(id, 255.0));
+    int r = int(id) / 255 / 255;
+    int g = (int(id) - b - r * 255 * 255) / 255;
+    return vec3(r, g, b) / 255.0;
+}
+
 void main() {
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
