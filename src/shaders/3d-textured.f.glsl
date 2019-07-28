ed3f7dcd3ca72a780ca9c74d21e5fa35b53f77e4
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-28 21:13:29 +0300

    And multiply object color if its index matches selected object index

--- src/shaders/3d-textured.f.glsl
+++ src/shaders/3d-textured.f.glsl
@@ -5,9 +5,10 @@ varying vec2 vTexCoord;
 varying vec3 vColor;
 
 uniform float renderIndices;
+varying vec4 vColorMultiplier;
 
 void main() {
-    gl_FragColor = texture2D(texture, vTexCoord * vec2(1, -1) + vec2(0, 1));
+    gl_FragColor = texture2D(texture, vTexCoord * vec2(1, -1) + vec2(0, 1)) * vColorMultiplier;
 
     if (renderIndices == 1.0) {
         gl_FragColor.rgb = vColor;
