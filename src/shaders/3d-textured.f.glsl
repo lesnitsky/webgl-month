02f1b604beb0ab553000271f7f3a20ea2aa52b08
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-07-29 17:30:50 +0300

    Now let's define a color of the fog (white). We can also pass this color to a uniform, but let's keep things simple

--- src/shaders/3d-textured.f.glsl
+++ src/shaders/3d-textured.f.glsl
@@ -11,6 +11,8 @@ varying float vFogAmount;
 void main() {
     gl_FragColor = texture2D(texture, vTexCoord * vec2(1, -1) + vec2(0, 1)) * vColorMultiplier;
 
+    vec3 fogColor = vec3(1.0, 1.0, 1.0);
+
     if (renderIndices == 1.0) {
         gl_FragColor.rgb = vColor;
     }
